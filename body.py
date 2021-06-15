ae489204d852839228076a70e53e46034718039d
DillonGrabskiInquiries <sahasrahlainquiries@gmail.com>
2021-06-15 00:53:53 -0500

    1

---
+++ body.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+
+counter = 0
+
+@app.route("/")
+    if counter == True
+    print(counter)
+
+
+@app.route("/destroy_session")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
